3bf67502130a99d522dd8b7c2ab4eb3d6029898f
Takao <sukisya70@gmail.com>
2019-01-14 14:18:13 +0900

    SSR

--- pages/Product.vue
+++ pages/Product.vue
@@ -1,3 +1,17 @@
 <template>
-  <h2>このページはNuxt社のトップのページです.</h2>
+  <h2>{{ title }}</h2>
 </template>
+
+<script>
+export default {
+  async asyncData (context) {
+    return await new Promise(resolve => {
+      setTimeout(() => {
+        resolve({
+          title: `このページはNuxt社のプロダクト情報のページです。(${ context.isServer ? 'サーバーサイドレンダリング' : 'クライアントサイドレンダリング' })`
+        })
+      }, 1000)
+    })
+  }
+}
+</script>
